581727bbe68ef788173e842503a5e844531127d3
efossier <evan.fossier@gmail.com>
2018-09-02 23:59:37 +0200

    Fix socket example

--- examples/socket.py
+++ examples/socket.py
@@ -1,8 +1,9 @@
 import pyproxy
 from pyproxy.socket import ProxyProtocolSocket
 
-SERVER_HOST = '9.9.9.9'
-SERVER_PORT = 9000
+# Client identity to send to the server via proxy-protocol
+MY_IP = '9.9.9.9'
+MY_PORT = 9000
 
 # Create a new socket that speaks proxy protocol
 sock = ProxyProtocolSocket(pyproxy.const.PROTOCOL_V1, src_addr=(SERVER_HOST, SERVER_PORT))
